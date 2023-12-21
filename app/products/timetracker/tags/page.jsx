b9d7be0e0cd59c4618dd7e772035ead6f429f83d
Wolferner <m.wolfern@yandex.com>
2023-12-21 17:34:28 +0200

    TagsDashboard:: create template

---
+++ app/products/timetracker/tags/page.jsx
@@ -0,0 +1,8 @@
+import TagsDashboard from "@/01 products/TimeTracker/TagsDashboard/TagsDashboard";
+export const metadata = {
+  title: "Project Management",
+};
+
+export default async function Page() {
+  return <TagsDashboard />;
+}
